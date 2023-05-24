2fe3cff2429ccb545679a3fbeb4a57c8e5fb14c7
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-05-24 17:36:46 +0530

    practice checkpoint.
    practiced till here.

---
+++ myworld/myTennisClus/members/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Member(models.Model):
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    phone = models.IntegerField(null=True)
+    joinedDate = models.DateField(null=True)
+
+
+    def __str__(self):
+        return f"{self.firstName} {self.lastName}"
+
+
+
+
